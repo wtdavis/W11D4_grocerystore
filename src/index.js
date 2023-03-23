448fba2448e4c4d6c233b09836944cccb8542be8
wtdavis <wtdavis@wesleyan.edu>
2023-03-23 14:31:08 -0400

    second

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import configureStore from './store';
+import {Provider} from 'react-redux';
+
+const store = configureStore()
+
+if (process.env.NODE_ENV !== "production") {
+  window.store = store
+}
 
 function Root() {
   return (
@@ -14,7 +22,9 @@ function Root() {
 
 ReactDOM.render(
   <React.StrictMode>
-    <Root />
+    <Provider store={store}>
+      <Root />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
