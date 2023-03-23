74d68ad309c2747bef3ce3f222bcc05acbefa08a
Charles Cruse <charle9@vt.edu>
2023-03-23 15:26:06 -0400

    finished phase 2

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import './index.css';
 import App from './App';
 import configureStore from './store';
 import {Provider} from 'react-redux';
+import { populateProduce } from './store/produce';
 
 const store = configureStore()
 
 if (process.env.NODE_ENV !== "production") {
   window.store = store
+  window.populateProduce = populateProduce
 }
 
 function Root() {
--- src/store/produce.js
+++ src/store/produce.js
@@ -1,16 +1,24 @@
 import produceData from '../mockData/produce.json'
 
+const POPULATE = 'produce/POPULATE';
+
+export const populateProduce = () => { 
+    return {
+    type: POPULATE,
+    produce: produceData
+}}
+
 export default function produceReducer( state = {}, action) {
+    const newState = {...state}
+    
     switch (action.type) {
+        case POPULATE:
+            action.produce.forEach(produce => {
+                newState[produce.id] = produce
+            })
+            return newState
         default: 
             return state;
     }
 }
 
-const POPULATE = 'produce/POPULATE';
-
-export const populateProduce = produce => { 
-    return {
-    type: POPULATE,
-    produce: produceData
-}}
